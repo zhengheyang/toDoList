25d30a8a8d7d691f7c49b50d938279b5815900d9
zhengheyang <zhenghe_yang@hotmail.com>
2017-07-24 18:06:23 +0800

    todolist-angular-practice

---
+++ js/controller.js
@@ -0,0 +1,28 @@
+angular.module('todoApp.controller', [])
+    .controller('test', function ($scope) {
+        $scope.usrInput = '';
+        $scope.todoList = [];
+        $scope.doneList = [];
+
+        $scope.addTodo = function () {
+            $scope.todoList.push({
+                text: $scope.usrInput,
+                done: false
+            });
+            $scope.usrInput = '';
+        };
+
+        $scope.doneThis = function (index) {
+            var arrObj = $scope.todoList.splice(index, 1);
+            $scope.doneList.push({
+                text: arrObj[0].text,
+                done: true
+            });
+        };
+
+        $scope.cancelThis = function (index) {
+            $scope.todoList.splice(index, 1);
+        }
+
+
+    })
